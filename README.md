879b81315c0cd040dcb434c626dfc6b8539ffadf
Kishore-Tall <kishoretallapally@gmail.com>
2025-02-11 20:54:27 +0530

    fixed the missing return statement in get_data route. And also added README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Xtract
-Extract data from multiple SEC financial filings in the simplest way possible
+# SEC Data Treasure
+Extract data from multiple SEC financial filings in the simplest way possible using APIs
+Extract compe data and directors data
+Extract data from other filings 
--- app.py
+++ app.py
@@ -10,23 +10,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/get-options', methods=['GET'])
-def get_options():
-    options = {
-        "companies": ["AAPL", "GOOG", "MSFT", "AMZN", "TSLA"],
-        "formtypes": ["10-K", "10-Q", "8-K", "S-1", "DEF 14A"],
-        "years": [str(year) for year in range(2000, 2025)]
-    }
-    return jsonify(options)
-
 @app.route('/get-data', methods=['POST'])
 def get_data():
     data = request.json
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    
     ticker = data.get('company')
     formtype = data.get('formtype')
     year = data.get('year')
-    
+
     process_input_and_download_reports(ticker, formtype, year)
+
+    return jsonify({"message": "Processing completed"}), 200  
+
     
     
 if __name__ == '__main__':
